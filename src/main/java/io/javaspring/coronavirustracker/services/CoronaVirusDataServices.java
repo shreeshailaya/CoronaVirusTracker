268d44ae05d6158432fcf42a4c385ed916253cc8
shreeshailaya <s.r.vitkar55@gmail.com>
2020-07-20 16:38:17 +0530

    Get set data

--- src/main/java/io/javaspring/coronavirustracker/services/CoronaVirusDataServices.java
+++ src/main/java/io/javaspring/coronavirustracker/services/CoronaVirusDataServices.java
@@ -2,6 +2,7 @@ package io.javaspring.coronavirustracker.services;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +17,7 @@ import java.net.http.HttpResponse;
 public class CoronaVirusDataServices {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
