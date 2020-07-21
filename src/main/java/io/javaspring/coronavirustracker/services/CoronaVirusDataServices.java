285fef0e0e60e66704a247484898624906756c68
shreeshailaya <s.r.vitkar55@gmail.com>
2020-07-21 23:35:21 +0530

    Modification

--- src/main/java/io/javaspring/coronavirustracker/services/CoronaVirusDataServices.java
+++ src/main/java/io/javaspring/coronavirustracker/services/CoronaVirusDataServices.java
@@ -14,7 +14,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.List;
+//import java.util.List;
 
 @Service
 public class CoronaVirusDataServices {
