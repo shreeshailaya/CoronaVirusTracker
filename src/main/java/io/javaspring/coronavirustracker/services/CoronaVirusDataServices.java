378c8eb97acc8b0970b4e7cfba3d3163bc194f96
shreeshailaya <s.r.vitkar55@gmail.com>
2020-07-21 23:37:40 +0530

    Schedule operation

--- src/main/java/io/javaspring/coronavirustracker/services/CoronaVirusDataServices.java
+++ src/main/java/io/javaspring/coronavirustracker/services/CoronaVirusDataServices.java
@@ -14,13 +14,14 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-//import java.util.List;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataServices {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     private List<LocationStats> allStats = new ArrayList<>();
-
+    @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         List<LocationStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
